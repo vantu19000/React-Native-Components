b2306656972e218fea7a75594e540a2d594d31b9
vantu19000 <vantu19000@gmail.com>
2021-01-07 17:33:38 +0700

    Drag drop

--- index.js
+++ index.js
@@ -3,8 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
 import {name as appName} from './app.json';
-import ImageSlider1 from "./src/Images/Slide1";
+import DragDropView from "./src/Animation/DragDropView";
 
-AppRegistry.registerComponent(appName, () => ImageSlider1);
+AppRegistry.registerComponent(appName, () => DragDropView);
